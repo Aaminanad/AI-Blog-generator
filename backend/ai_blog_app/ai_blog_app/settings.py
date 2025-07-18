67916c1bf1fe6cd01b11d2732372e1e26e038b88
aaminanadem <aaminanadem@gmail.com>
2025-07-18 14:39:20 +0500

    included erro msgs

--- backend/ai_blog_app/ai_blog_app/settings.py
+++ backend/ai_blog_app/ai_blog_app/settings.py
@@ -70,14 +70,19 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ai_blog_app.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.qcyhrcdbrrtuuturqqsy',
+        'PASSWORD': 'Aaminanadism',
+        'HOST': 'aws-0-ap-south-1.pooler.supabase.com',
+        'PORT': '5432',
+    
+      
+   
+                }
 }
 
 
