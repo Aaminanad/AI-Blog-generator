0a815ffca309155f14374e97e3ba9ec65f053545
aaminanadem <aaminanadem@gmail.com>
2025-07-19 20:31:23 +0500

    auth stuff done

--- backend/ai_blog_app/blog_generator/views.py
+++ backend/ai_blog_app/blog_generator/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def index(request):
     return render(request, 'index.html')
 def user_login(request):
@@ -42,4 +44,5 @@ def user_signup(request):
         
     return render(request, 'signup.html')    
 def user_logout(request):
-    pass
+    logout(request)
+    return redirect('/')         
