2569e5f75aeb1450c0f8447d4b4c90f82f35a84b
aaminanadem <aaminanadem@gmail.com>
2025-07-20 15:22:45 +0500

    yt link

--- backend/ai_blog_app/blog_generator/views.py
+++ backend/ai_blog_app/blog_generator/views.py
@@ -3,29 +3,47 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.csrf import csrf_extempt
+from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.conf import settings
 import json
-
+from pytube import YouTube
 # Create your views here.
 @login_required
 def index(request):
     return render(request, 'index.html')
-@csrf_extempt
+@csrf_exempt
 def generate_blog(request):
-    if request.method =='POST':
+    if request.method =='POST': #when POST method is used csrf is important
         try:
             data= json.loads(request.body)
-            yt_link=data['link']
+            yt_link= data['link']
+            return JsonResponse({'content': yt_link})
         except(KeyError, json.JSONDecodeError):
             return JsonResponse({'error':'Invalid data sent'}, status=400)
         #get yt title
+        title= yt_title(yt_link)
         # get transcript
+
         #use open ai to generate the blog
         # save blog article to database
-        # return blog article as a response     
+        # return blog article as a response
+            
     else:
         return JsonResponse({'error':'Invalid request method'}, status=405)
+
+def yt_title(link):
+    yt= YouTube(link)
+    title= yt.title
+    return title
+def download_audio(link):
+    yt=YouTube(link)
+    video=yt.streams.filter(only_audio=True).first()
+    out_file = video.download(output_path=settings.MEDIA_ROOT)
+def get_transcription(link):
+    #audio_file= pass
+    pass
+
 
 def user_login(request):
     if request.method == 'POST':
